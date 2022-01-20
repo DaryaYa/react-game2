3db9020cbf39444a6c6e671471866a648c88c9f2
DaryaYa <lyalyaka@mail.ru>
2022-01-20 12:26:26 +0300

    fix: env

--- src/components/BGButton.jsx
+++ src/components/BGButton.jsx
@@ -1,11 +1,10 @@
+import React from "react";
 import { StyledStartButton } from "./styles/StyledStartButton";
-const UNSPLASH_KEY =
-  "7f3414d36c39132ae4aae83156a242d1c6ce22dbadf927f69b5e1ddbb4c2842c";
 
 export const BGButton = () => {
 
 	function getLinkToImage() {
-    const url = `https://api.unsplash.com/photos/random?query=nature&client_id=${UNSPLASH_KEY}`;
+    const url = `https://api.unsplash.com/photos/random?query=nature&client_id=${process.env.REACT_APP_UNSPLASH_KEY}`;
     fetch(url)
       .then((res) => res.json())
       .then((data) => {
